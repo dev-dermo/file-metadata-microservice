9c99af4904ff9cb755cb238d3abb7caade689292
Diarmuid Murphy <diarmuid.com@gmail.com>
2017-12-11 18:27:12 -0800

    all done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var express = require('express');
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -10,6 +12,11 @@ app.set('views', path.resolve(__dirname, 'views'));
 
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.post('/get-file-size', upload.single('file'), function (req, res) {
+	var result = { bytes: req.file.size };
+	res.json(result);
 });
 
 app.listen(app.get('port'), function() {
